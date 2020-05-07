f599e7d5c64338ab825f55da2dcc5e7477b9b422
masTech Developer <ashir342@gmail.com>
2020-05-08 03:08:44 +0500

    github Repos

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null,
   };
@@ -53,6 +54,16 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // User Repos Get API
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
   // Users Clear State
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -64,7 +75,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, repos, user, loading } = this.state;
 
     return (
       <Router>
@@ -97,7 +108,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
