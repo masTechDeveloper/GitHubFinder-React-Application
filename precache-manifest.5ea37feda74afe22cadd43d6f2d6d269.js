803712075e681a2899662b50ad8467d0422ccb1f
masTech Developer <ashir342@gmail.com>
2020-05-15 17:19:56 +0500

    Updates

---
+++ precache-manifest.5ea37feda74afe22cadd43d6f2d6d269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956b70419e2566c015d5cedbde676c26",
+    "url": "/github-finder-react-application/index.html"
+  },
+  {
+    "revision": "639d2090f7b7d4fef36d",
+    "url": "/github-finder-react-application/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "5a80981daf791f8f7487",
+    "url": "/github-finder-react-application/static/js/2.14f34225.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-finder-react-application/static/js/2.14f34225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "639d2090f7b7d4fef36d",
+    "url": "/github-finder-react-application/static/js/main.6530a303.chunk.js"
+  },
+  {
+    "revision": "9775e5b6c509b3d5c207",
+    "url": "/github-finder-react-application/static/js/runtime-main.54e1cbaa.js"
+  }
+]);
